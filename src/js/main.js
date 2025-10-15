e158f40d15fc5b949c1b29132dc080f93bb5efb4
Nelson Ruiz <martineznelson515@gmail.com>
2025-10-15 11:29:10 -0600

    implementacion de fuente de texto montserrat, e interactividad con el navbar

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,18 @@ $btn_hamburger = d.querySelector(".nav-hamburger");
 $btn_hamburger.addEventListener("click", e=>{
     $ul.classList.toggle("hidden");
 
-})
+});
 
+// Cerrar el navbar cuando se apreta algun link
+$ul.addEventListener("click", e =>{
+    if(e.target.matches("a")){
+        $ul.classList.add("hidden")
+    }
+});
 
-
+// cerrar el navbar cuando apretamos fuera del navbar xd
+d.addEventListener("click", e=>{
+    if(!$ul.contains(e.target) && !$btn_hamburger.contains(e.target)){
+        $ul.classList.add("hidden")
+    }
+});
